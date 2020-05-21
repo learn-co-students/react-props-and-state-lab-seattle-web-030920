ade9b18ce425fe3a674d66bea316274e1007c90d
github username <github email address>
2020-05-21 00:09:11 -0700

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,35 @@ class App extends React.Component {
     }
   }
 
+  fetchPets = () => {
+    const { type } = this.state.filters
+    fetch(`/api/pets${type === 'all' ? "":'?type='+type}`  )
+    .then(resp => resp.json())
+    .then((data => this.setState({pets:data})))
+  }
+
+  adoptPet = (currentPetId) => {
+    this.setState(prevStat => {
+      return prevStat.pets.map(pet => {
+        if (pet.id !== currentPetId) {
+          return pet
+        } else {
+          pet.isAdopted = true
+          return pet
+        }
+      })
+    })
+  }
+
+  changeType = (selection) => {
+    console.log(selection)
+    this.setState({
+      filters: {
+        type: selection
+      }
+    })
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +53,14 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters 
+              onChangeType={this.changeType} 
+              onFindPetsClick={this.fetchPets}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser 
+              onAdoptPet={this.adoptPet}
+              pets={this.state.pets}/> 
             </div>
           </div>
         </div>
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -2,9 +2,21 @@ import React from 'react'
 
 import Pet from './Pet'
 
+
 class PetBrowser extends React.Component {
+
+  // renderPets = () => {
+  //  return this.props.pets.map((pet) => {
+  //    return <Pet pet={pet} key={pet.id}/>
+  //  })
+  // }
+
+  renderPets = () => this.props.pets.map((pet) => 
+    <Pet onAdoptPet={this.props.onAdoptPet} pet={pet} key={pet.id}/>
+  )
+
   render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+    return <div className="ui cards">{this.renderPets()}</div>
   }
 }
 
